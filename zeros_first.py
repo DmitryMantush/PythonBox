f5b006df27aa1838036e5a3dd37b5fdedfe1668b
DmitryMantush <brownmittens@gmail.com>
2019-07-26 18:10:21 +0300

    Less code to do the same.

--- zeros_first.py
+++ zeros_first.py
@@ -2,15 +2,10 @@ print('How many numbers do you want to add?')
 N = int(input())
 s = []
 for i in range(N):
-    s.append(int(input()))
-count = 0
-for item in s:
-    if item == 0:
-        count += 1
-if count > 0:
-    for a in range(count):
-        s.remove(0)
-    for n in range(count):
+    z = int(input())
+    if z == 0:
         s.insert(0, 0)
-    print('Zeros were moved in front of the other numbers.')
+    else:
+        s.append(z)
+print('Zeros were moved in front of the other numbers.')
 print(s)
