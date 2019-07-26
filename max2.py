61c71a2f9e45955ad9e5a31bc1904dd86ce51718
DmitryMantush <brownmittens@gmail.com>
2019-07-26 17:29:27 +0300

    Without creating a list.

--- max2.py
+++ max2.py
@@ -1,19 +1,12 @@
 print('How many numbers do you want to add?')
 N = int(input())
-s = []
+z1 = 0
+z2 = 0
 for i in range(N):
-    s.insert(0, int(input()))
-#ss = sorted(s, reverse=True)
-#print('These guys are the biggest:', ss[0], ',', ss[1], end='.')
-#Выше первый рабочий вариант, но он содает копию исходного списка.
-m1 = max(s)
-if s[0] == m1:
-    m2 = s[1]
-else:
-    m2 = s[0]
-for z in s:
-    if z == m1:
-        continue
-    if z > m2:
-        m2 = z
-print('These guys are the biggest:', m1, ',', m2, end='.')
+    b = int(input())
+    if b > z1:
+        z2 = z1
+        z1 = b
+    elif z1 > b > z2:
+        z2 = b
+print(z1, z2)
